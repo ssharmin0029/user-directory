ea0f36164d3054bcb4073590a26c6730d7ced9ee
ssharmin0029 <sabrina.sharmin29@myhunter.cuny.edu>
2021-06-23 10:52:34 -0400

    App.js + App.css modified & App.test + logo.svg + setupTests remvoed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-// import logo from './logo.svg';
 import './App.css';
-// *****
 import API from './utils/API';
-// *****
 
 function App() {
 
@@ -17,29 +14,8 @@ function App() {
   return (
     <div className="App">
       <button onClick={users}></button>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
 
 export default App;
-
-// Lifecycle methods 
-// Class Component didMount
-
-// Data set to State
-
-// if using function component then use hooks 
